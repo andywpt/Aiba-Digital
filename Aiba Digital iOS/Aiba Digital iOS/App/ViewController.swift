d648cb49b222626a9e111c2d190d3ca684a239cd
Andy Wu <130017974+andywpt@users.noreply.github.com>
2023-12-27 14:55:23 +0800

    Feature/initial setup (#1)
    * Set up Xcode project
    
    * Set up Xcode and Firebase project
    
    ---------
    
    Co-authored-by: Andy Wu <andywu@Andys-MacBook-Pro.local>

---
+++ Aiba Digital iOS/Aiba Digital iOS/App/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Aiba Digital iOS
+//
+//  Created by Andy Wu on 2023/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+
+}
+
